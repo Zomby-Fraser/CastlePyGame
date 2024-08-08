0dbad3895c98aa669a9c6d5f19a80d31642349c9
Zomby-Fraser <78499426+Zomby-Fraser@users.noreply.github.com>
2024-08-08 16:28:46 -0400

    Update game_config.py

--- revision/game_config.py
+++ revision/game_config.py
@@ -1,6 +1,7 @@
 class GameConfig:
     
-    def __init__(self, face_down_castle_cnt = 3, hand_size = 3):
+    def __init__(self, castle_slots = 3, face_down_castle_cnt = 3, hand_size = 3):
+        self.castle_slots = castle_slots # Need error handling for when face_down_castle_cnt % castle_slots != 0
         self.face_down_castle_cnt = face_down_castle_cnt
         self.hand_size = hand_size
         self.starting_hand_size = face_down_castle_cnt + hand_size
