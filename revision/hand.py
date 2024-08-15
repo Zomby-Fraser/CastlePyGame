754577e2d00eea978095ab6671200fbe3fb1a546
Zomby-Fraser <78499426+Zomby-Fraser@users.noreply.github.com>
2024-08-15 14:51:45 -0400

    Update hand.py

--- revision/hand.py
+++ revision/hand.py
@@ -2,4 +2,33 @@ class Hand:
     
     def __init__(self, player_id):
         self.cards = []
+        self.playable_cards = []
         self.player_id = player_id
+
+    def listPlayableCards(self, pile):
+        self.playable_card = []
+        for card in self.cards:
+            if isCardPlayable(card, pile):
+                self.playable_card.append(card)
+
+    '''
+        Determines if a particular card is playable at that moment in the game.
+        INPUTS -> 
+            * card -> Class Card
+                - The card being checked for playability
+            * pile -> Class Pile
+                - The active pile in the game
+        RETURNS -> Bool
+    '''
+    def isCardPlayable(card, pile):
+        pile_top_card = pile.cards[-1]
+        if card.is_special:
+            return True
+        elif pile_top_card.special_property == 'copy':
+            pile_top_non_copy = pile.nonCopyValue(pile, -1)
+            if pile_top_card.special_property == 'reset':
+                return True
+            elif pile_top_card.special_property == 'lower':
+                pile_top_non_copy.value > card.value
+
+   
