6d19b79f6b6380b7458e7a7434c7afb1163138fd
Zomby-Fraser <78499426+Zomby-Fraser@users.noreply.github.com>
2024-08-15 14:29:56 -0400

    Update card.py

--- revision/card.py
+++ revision/card.py
@@ -2,12 +2,14 @@ class Card:
     
     _next_id = 1  # Class variable to assign unique IDs to each card
 
-    def __init__(self, suit, rank, value):
+    def __init__(self, suit, rank, value, is_special, special_property):
         self.id = Card._next_id
         Card._next_id += 1
         self.suit = suit
         self.rank = rank
         self.value = value
+        self.is_special = is_special
+        self.special_proprety = special_property
         
         def __str__(self):
             return f'Card #{self.id}: {self.rank} of {self.suit} with value {self.value}'
