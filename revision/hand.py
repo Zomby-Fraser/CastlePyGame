5c7b9669d394cd5493d8f6413c6a75596f2e5c7c
Zomby-Fraser <78499426+Zomby-Fraser@users.noreply.github.com>
2024-08-15 15:01:34 -0400

    Update hand.py

--- revision/hand.py
+++ revision/hand.py
@@ -5,6 +5,13 @@ class Hand:
         self.playable_cards = []
         self.player_id = player_id
 
+    '''
+        Returns a list of playable cards in the player's hand
+        INPUTS ->
+            * pile -> Class Pile
+                - The active pile in the game
+        RETURNS -> None; alters the class attribute
+    '''
     def listPlayableCards(self, pile):
         self.playable_card = []
         for card in self.cards:
@@ -12,7 +19,7 @@ class Hand:
                 self.playable_card.append(card)
 
     '''
-        Determines if a particular card is playable at that moment in the game.
+        Determines if a particular card is playable at that moment in the game
         INPUTS -> 
             * card -> Class Card
                 - The card being checked for playability
@@ -20,15 +27,34 @@ class Hand:
                 - The active pile in the game
         RETURNS -> Bool
     '''
-    def isCardPlayable(card, pile):
+    def isCardPlayableCopyCheck(card, pile):
         pile_top_card = pile.cards[-1]
         if card.is_special:
             return True
         elif pile_top_card.special_property == 'copy':
             pile_top_non_copy = pile.nonCopyValue(pile, -1)
-            if pile_top_card.special_property == 'reset':
-                return True
-            elif pile_top_card.special_property == 'lower':
-                pile_top_non_copy.value > card.value
+            return self.isCardPlayable(pile_top_non_copy, card)
+        else:
+            return self.isCardPlayable(pile_top_card, card)
+            
+    '''
+        Determines if a particular card in general (internal use only, see `Hand.isCardPlayableCopyCheck`)
+        INPUTS -> 
+            * pile_card -> Class Card
+                - The card being compared to.
+            * hand_card -> Class Card
+                - The card being checked for playability
+        RETURNS -> Bool
+    '''
+    def isCardPlayable(self, pile_card, hand_card):
+        if reference_card.special_property == 'reset':
+            return True
+        elif reference_card.special_property == 'lower' and reference_card.value > hand_card.value:
+            return True
+        elif reference_card.value <= hand_card.value:
+            return True
+        else:
+            return False
+                    
 
    
